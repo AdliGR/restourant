564cf2218e07eece512fb5d0f413b3d1e2fd1058
DavidTheStudent <127299373+DavidTheMarksmen@users.noreply.github.com>
2023-10-23 21:15:20 +0700

    Update register.php
    Pls review idk if this is good or not 😭 :

--- register.php
+++ register.php
@@ -107,6 +107,7 @@
                             </p>
                         </div>
 
+                        <!--//Becoming Admin//-->
                         <div class="form-group">
                             <input type="checkbox" name='admin' value="Admin"> Cek box ini untuk menjadi Admin.
                         </div>
