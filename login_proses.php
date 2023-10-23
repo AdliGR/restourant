700b851c28fa72e2549ef3c88d2914548752cf53
Eliezer223 <148566130+Eliezer223@users.noreply.github.com>
2023-10-24 00:26:59 +0700

    Updated

--- login_proses.php
+++ login_proses.php
@@ -1,31 +1,51 @@
 <?php
 session_start();
 
-if (isset($_POST['username_email']) && isset($_POST['password']) && isset($_POST['captcha'])) {
-    $username_email = $_POST['username_email'];
-    $password = $_POST['password'];
-    $captcha = $_POST['captcha'];
+$servername = "localhost";
+$username = "username_db"; // Ganti dengan username database Anda
+$password = "password_db"; // Ganti dengan password database Anda
+$dbname = "uts_restorant";
 
-    if (strtolower($captcha) == strtolower($_SESSION['captcha'])) {
-        // Validasi login, misalnya dengan database
-        if ($username_email == 'admin' && $password == 'adminpass') {
-            // Admin berhasil login
-            $_SESSION['admin'] = true;
-            header('Location: admin_dashboard.php');
-        } elseif ($username_email == 'customer' && $password == 'customerpass') {
-            // Customer berhasil login
-            $_SESSION['customer'] = true;
-            header('Location: customer_dashboard.php');
-        } else {
-            // Peringatan jika username/email atau password salah
-            header('Location: login.php?error=1');
-        }
-    } else {
-        // Peringatan jika Captcha salah
-        header('Location: login.php?error=2');
-    }
-} else {
-    // Peringatan jika tidak ada username/email, password, atau captcha
-    header('Location: login.php?error=3');
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $captcha = $_POST["captcha"];
+
+    // Verifikasi Captcha
+    if ($_SESSION["captcha"] != $captcha) {
+        echo "Captcha salah. Coba lagi.";
+    } else {
+        // Proses login
+        $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['role'] = $row['role'];
+            
+            if ($row['role'] == 'admin') {
+                // Redirect ke halaman admin
+                header("Location: admin_crud.php");
+            } elseif ($row['role'] == 'customer') {
+                // Redirect ke halaman pemesanan
+                header("Location: customer_order.php");
+            } else {
+                echo "Role tidak valid.";
+            }
+        } else {
+            echo "Username atau password salah.";
+        }
+    }
+}
+
+$conn->close();
 ?>
